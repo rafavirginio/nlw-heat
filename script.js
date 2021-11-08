bfcfab9ffdef5fec0cf9bac12cee1e69223e6f40
Rafa Virginio <rafael5845@gmail.com>
2021-11-07 23:26:59 -0300

    update index and js

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-const github = 'rafavirginio'
-const youtube = 'UCoBEQHHeIgphjsdka3VoHzw'
-const instagram = 'rafavirgiinio'
-const facebook = '100001573343039'
-const linkedin = 'rafael-virgínio-da-silva-288a8468'
+const linksSocialMedia = {
+  github: 'rafavirginio',
+  youtube: 'channel/UCoBEQHHeIgphjsdka3VoHzw',
+  instagram: 'rafavirgiinio',
+  facebook: '100001573343039',
+  linkedin: 'in/rafael-virgínio-da-silva-288a8468'
+}
+
+function changeSocialMediaLinks() {
+  //Por padrão o js busca por uma id quando não for utilizado "getElementby..."
+  // userName.textContent = 'Rafa'
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+
+    alert(li.children[0].href)
+  }
+}
+
+changeSocialMediaLinks()
