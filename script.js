2baae12c6aad0da9492230d548f99e68adef71f7
Rafa Virginio <rafael5845@gmail.com>
2021-11-09 15:49:46 -0300

    update card contents

--- script.js
+++ script.js
@@ -7,15 +7,29 @@ const linksSocialMedia = {
 }
 
 function changeSocialMediaLinks() {
-  //Por padrão o js busca por uma id quando não for utilizado "getElementby..."
-  // userName.textContent = 'Rafa'
+  //Por padrão o js busca por uma id pela DOM quando não for utilizado "getElementby..."
   for (let li of socialLinks.children) {
     const social = li.getAttribute('class')
 
     li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
-
-    alert(li.children[0].href)
   }
 }
 
 changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+  //  fetch armazena um dado mas não sabe que é um JSON
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      //textContent troca todo o conteudo da tag
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
